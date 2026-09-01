3343dfdf2dce52ebaf6740ea31fa6c637ad69e4c
nishadtardalkar <nishadtardalkar97@gmail.com>
2026-09-01 17:11:08 -0400

    install rust

--- scripts/envs.sh
+++ scripts/envs.sh
@@ -3,3 +3,7 @@
 export TMPDIR=/rsstu/users/d/dslalush/KneeProject/.cache/tmp
 export RUSTUP_HOME=/rsstu/users/d/dslalush/KneeProject/.cache/rustup
 export CARGO_HOME=/rsstu/users/d/dslalush/KneeProject/.cache/cargo
+
+mkdir -p $TMPDIR $RUSTUP_HOME $CARGO_HOME
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
